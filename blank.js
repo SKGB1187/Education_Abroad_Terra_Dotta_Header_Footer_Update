f5b64f67f604a6342ea2de0ce89fc8bfc62468ee
Sara <155405968+SKGB1187@users.noreply.github.com>
2025-08-26 13:12:37 -0600

    Files for Terra Dotta Upload
    Web Express Header Footer update for Terra Dotta 08262025

---
+++ blank.js
@@ -0,0 +1,4 @@
+// This is where the javascript gets included
+//$(document).ready(function () {
+//    $("#top-navbar > ul").append('<li><a href="https://www.colorado.edu/abroad/get-started">Get Started</a></li>');
+//});
